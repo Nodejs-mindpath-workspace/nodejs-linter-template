f2b8310081f1fcf81a74bf61203330197600e159
Vinay Panwar <vinay@mindpathtech.com>
2025-02-27 04:49:39 +0000

    FEAT: Updated the eslint version and used new flat config style.

--- src/swaggers/constants/express.ts
+++ src/swaggers/constants/express.ts
@@ -2,6 +2,6 @@ import ExpressConstants from "../types/constants/express";
 
 const expressConstants: ExpressConstants = {
     REQUEST_PARAMETERS: ["body", "params", "query", "file"],
-}
+};
 
 export default expressConstants;
